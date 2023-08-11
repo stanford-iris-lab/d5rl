ccec343cb8d99de69b904d16eba2228e7ad5959a
Kyle Hatch <khatch@svl14.stanford.edu>
2023-08-10 17:02:53 -0700

    added instructions to readme

---
+++ jaxrl2/agents/__init__.py
@@ -0,0 +1,9 @@
+from jaxrl2.agents.bc import BCLearner
+from jaxrl2.agents.drq import DrQLearner
+from jaxrl2.agents.iql import IQLLearner
+from jaxrl2.agents.pixel_bc import PixelBCLearner
+from jaxrl2.agents.pixel_iql import PixelIQLLearner
+from jaxrl2.agents.sac import SACLearner
+from jaxrl2.agents.pixel_cql import PixelCQLLearner ###===###
+from jaxrl2.agents.pixel_ddpm_bc import PixelDDPMBCLearner ###===###
+from jaxrl2.agents.pixel_idql.pixel_idql_learner import PixelIDQLLearner ###===###
